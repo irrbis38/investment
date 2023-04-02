52fea4c394b6c374c60a47cc03283d0c483e4588
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-04-02 11:08:37 +0300

    feat: public site on gh-pages

--- sources/src/js/_libs_concat.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  "./src/libs/gsap/gsap.min.js",
-  "./src/libs/gsap/GSDevTools.min.js",
-  "./src/libs/gsap/ScrollTrigger.min.js",
-  "./src/libs/gsap/ScrollToPlugin.min.js",
-];
