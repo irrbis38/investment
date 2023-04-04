61aaa1a38d26a428ae5bcb2f1fcce26f8d176d9a
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-04-04 16:25:46 +0300

    feat: add swiper slider lib

--- sources/src/js/_libs_concat.cjs
+++ sources/src/js/_libs_concat.cjs
@@ -3,4 +3,5 @@ module.exports = [
   "./src/libs/gsap/GSDevTools.min.js",
   "./src/libs/gsap/ScrollTrigger.min.js",
   "./src/libs/gsap/ScrollToPlugin.min.js",
+  "./src/libs/swiper/swiper-bundle.min.js",
 ];
